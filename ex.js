7b5b4939dc40be0fa2de31780b4cb5be260695b4
KimTaeYun <sbarrys@ajou.ac.kr>
2020-02-04 10:23:47 +0900

    node배열연습

---
+++ ex.js
@@ -0,0 +1,34 @@
+var cars =[];
+var car01={
+	name:"sonata",
+	ph:"500ph",
+	start : function(){
+		console.log("engine is starting");
+	},
+	stop : function(){
+		console.log("engine is stop");
+	}
+}
+var car02={
+	name:"sonata2",
+	ph:"500ph",
+	start : function(){
+		console.log("engine is starting");
+	},
+	stop : function(){
+		console.log("engine is stop");
+	}
+}
+var car03={
+	name:"sonata3",
+	ph:"500ph",
+	start : function(){
+		console.log("engine is starting");
+	},
+	stop : function(){
+		console.log("engine is stop");
+	}
+}
+cars[0]=car01;
+cars[1]=car02;
+console.log(cars);

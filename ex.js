9c748896de71d316bbc69469cbd38b556f9528ad
KimTaeYun <sbarrys@ajou.ac.kr>
2020-02-04 11:08:46 +0900

    동기비동기 개념

--- ex.js
+++ ex.js
@@ -1,42 +1,18 @@
-var cars =[];
-var car01={
-	name:"sonata",
-	ph:"500ph",
-	start : function(){
-		console.log("engine is starting");
-	},
-	stop : function(){
-		console.log("engine is stop");
-	}
-}
-var car02={
-	name:"sonata2",
-	ph:"500ph",
-	start : function(){
-		console.log("engine is starting");
-	},
-	stop : function(){
-		console.log("engine is stop");
-	}
-}
-var car03={
-	name:"BMW",
-	ph:"333",
-	start : function(){
-		console.log("engine is starting");
-	},
-	stop : function(){
-		console.log("engine is stop");
-	}
-}
-cars[0]=car01;
-cars[1]=car02;
-cars[2]=car03;
+var fs=require('fs');
 
-for(var i=0;i<cars.length;i++){
-	var element= cars[i];
-	if(element.name=="BMW"){
-		console.log(element);
-	}
-	
-}
+console.log("첫번째 기능");
+// fs.readFile('test.txt','utf8',function(err,result){
+// 	if(err){
+// 		console.error(err);
+// 		throw err;
+
+// 	}
+// 	else{
+// 		console.error("두번쨰기능");
+// 		console.log(result);
+// 	}
+// });
+ var result= fs.readFileSync('test.txt','utf8');
+
+console.log(result);
+console.log('마지막기능');

4392f0a72173e670cc8009f4fe1d8154f21c58e0
KimTaeYun <sbarrys@ajou.ac.kr>
2020-02-04 11:18:55 +0900

    동기비동기 개념3

--- ex.js
+++ ex.js
@@ -1,25 +1,20 @@
 var fs=require('fs');
 
-function aFunc(){
+function callbackFunc( callback ){
 
-	setTimeout(function(){
-		console.log('a');},700
-	);
+	fs.readFile('test.txt','utf8',function(err,result){
+		if(err){console.error(err);
+		throw err;
+		}
+		else{
+			console.error("두번째기능");;
+			callback(result);
+		}
+	});
 }
+console.log("A");
+callbackFunc(function(data){
+	console.log(data);
+	console.log("c");
 
-function bFunc(){
-
-	setTimeout(function(){
-		console.log('b');},100
-	);
-}
-
-function cFunc(){
-
-	setTimeout(function(){
-		console.log('c');},700
-	);
-}
-cFunc();
-bFunc();
-aFunc();
+});

27d0e0237256f5c4f7fb0f10dc78ffbb672920bc
KimTaeYun <sbarrys@ajou.ac.kr>
2020-02-05 14:17:59 +0900

    finished GetAuthorizationCodeAndGetTokenandSignUp

---
+++ expressHttpServer.js
@@ -0,0 +1,65 @@
+const express=require('express');
+const app=express();
+var request = require('request')
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'kty',
+  password : 'wmf1284546',
+  database : 'fintechExam',
+  port :"3306"
+});
+
+app.set('views',__dirname+'/views');  //이안에 들어있는 파일들은 뷰파일이다.
+app.set('view engine','ejs');//
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.get("/signup",function(req,res){
+    res.render('signup');
+})
+app.post("/signup",function(req,res){
+  var userName= req.body.userName;
+  var userEmail=req.body.userEmail;
+  var userPassword=req.body.userPassword;
+  var userAccessToken=req.body.userAccessToken;
+  var userRefreshToken=req.body.userRefreshToken;
+  var userSeqNo=req.body.userSeqNo;
+
+  var sql = "INSERT INTO user (email, password, name, accesstoken, refreshtoken, userseqno) VALUES (?,?,?,?,?,?)"
+  connection.query(sql,[userEmail, userPassword, userName, userAccessToken, userRefreshToken, userSeqNo], function (err, results, fields) {
+    if(err){
+      console.error(err);
+      throw err;
+    }
+    else {
+      res.json(1);
+    }
+  });
+
+ });
+ 
+app.get('/authResult', function(req, res){
+    var authCode = req.query.code;
+    console.log(authCode);
+    var option = {
+      method : "POST",
+      url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+      headers : {
+        'Content-Type' : "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      form : {
+          code : authCode,
+          client_id : 'q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF',
+          client_secret : 'yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy',
+          redirect_uri : 'http://localhost:3000/authResult',
+          grant_type : 'authorization_code'
+      }
+    }
+    request(option, function (error, response, body) {
+      var parseData = JSON.parse(body);
+      res.render('resultChild',{data : parseData})
+    });
+  })
+app.listen(3000);

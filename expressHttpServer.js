8f4dad36ee4b0ef3ae41703aa6fd4bf617cbb609
KimTaeYun <sbarrys@ajou.ac.kr>
2020-02-06 15:45:50 +0900

    잔액조회, 거래내역조회 완료 큐알코드.js 라이브러리 이용해서 작성 완료

--- expressHttpServer.js
+++ expressHttpServer.js
@@ -3,6 +3,9 @@ const app=express();
 var request = require('request');
 var mysql      = require('mysql');
 const jwt= require('jsonwebtoken');
+//미들웨어 사용
+var auth = require('./lib/auth');
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'kty',
@@ -20,6 +23,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.get('/qrcode',function(req,res){
+  res.render('qrcode');
+
+})
+app.get('/',function(req,res){
+  res.render('main');
+})
 app.get('/signup', function(req, res){
   res.render('signup');
 })
@@ -27,6 +37,104 @@ app.get('/signup', function(req, res){
 app.get('/login', function(req, res){
   res.render('login');
 })
+
+app.get('/list',auth,function(req,res){
+  var user=req.decoded;
+  var sql="SELECT * FROM user WHERE id= ?";
+  connection.query(sql,[user.userId],function(err,results,fields){
+    if(err){
+      console.error(err);
+      throw err;
+    }else{
+      var option={
+        method:'GET',
+        url:"https://testapi.openbanking.or.kr/v2.0/user/me",
+        headers:{
+          'Authorization':"Bearer"+results[0].accesstoken
+        },
+        qs:{
+          'user_seq_no':'1100753046'
+        }
+      }
+      
+      request(option,function(error,response,result){
+        var a=    JSON.parse(result) 
+        res.json(a);
+      });
+
+    }
+  })
+
+})
+app.get('/balance',function(req,res){
+  res.render('balance')
+})
+app.post("/balance",auth,function(req,res){
+  var user = req.decoded;
+  console.log(user.userName + "접속하여 잔액조회를 합니다.");
+  var finusernum = req.body.fin_user_num
+  var countnum = Math.floor(Math.random() * 1000000000) + 1;
+  var transId = "T991608540U" + countnum;
+  var sql = "SELECT * FROM user WHERE id = ?"
+  connection.query(sql,[user.userId], function (err, results, fields) {
+    var option = {
+      method : "GET",
+      url : "https://testapi.openbanking.or.kr/v2.0/account/balance/fin_num",
+      headers : {
+        'Authorization' : "Bearer " + results[0].accesstoken
+      },
+      qs : {
+        bank_tran_id : transId,
+        fintech_use_num : finusernum,
+        tran_dtime : "20200206132847"
+      }
+    };
+    request(option, function (error, response, body) {
+      var parseData = JSON.parse(body);
+      res.json(parseData);
+    });
+  });
+ })
+
+
+ app.post('/transactionlist',auth, function(req, res){
+  console.log(req);
+  var user = req.decoded;
+  console.log(user.userName + "접속하여 잔액조회를 합니다.");
+  var finusenum = req.body.fin_user_num
+  console.log(finusenum);
+  var countnum = Math.floor(Math.random() * 1000000000) + 1;
+  var transId = "T991608540U" + countnum;
+  var sql = "SELECT * FROM user WHERE id = ?"
+  connection.query(sql,[user.userId], function (err, results, fields) {
+    var option = {
+      method : "GET",
+      url : "https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num",
+      headers : {
+        'Authorization' : "Bearer " + results[0].accesstoken
+      },
+      qs : {
+        bank_tran_id :  transId,
+        fintech_use_num : finusenum,
+        inquiry_type : 'A',
+        inquiry_base : 'D',
+        from_date : '20200206',
+        to_date : '20200206',
+        sort_order : 'D',
+        tran_dtime : "20200205172120"
+      }
+    };
+    
+    request(option, function (error, response, body) {
+      var parseData = JSON.parse(body);
+      res.json(parseData);
+    });
+  });
+})
+
+
+
+
 
 app.get('/authResult', function(req, res){
   var authCode = req.query.code;
@@ -39,8 +147,8 @@ app.get('/authResult', function(req, res){
     },
     form : {
         code : authCode,
-        client_id : 'q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF',
-        client_secret : 'yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy',
+        client_id : 'A7DQhWnLZ27cmS36sbbeIrz4x8Ml6qr2jKbs9n6f',
+        client_secret : 'WoND1cu6CaFARfzW8Sl2SmhGU7JFGWTa0FhBmJ9Q',
         redirect_uri : 'http://localhost:3000/authResult',
         grant_type : 'authorization_code'
     }
@@ -73,7 +181,7 @@ app.post('/signup', function(req, res){
 app.post('/login', function(req, res){
   var userEmail = req.body.userEmail;
   var userPassword =req.body.userPassword;
-  console.log(req.body);
+  console.log(userEmail);
   var sql = "SELECT * FROM user WHERE email = ?"
   connection.query(sql,[userEmail], function(err, results){
     if(err){
@@ -111,5 +219,11 @@ app.post('/login', function(req, res){
     }
   })
 })
+
+
+app.get('/authTest',auth, function(req, res){
+  res.json("메인 컨텐츠");
+})
+
 
 app.listen(3000)
